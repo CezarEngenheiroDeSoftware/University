ce506b2cc32bf84402f5aaf55c15383121062c95
CezarEngenheiroDeSoftware <cezar.audelania@gmail.com>
2025-01-25 10:34:23 -0300

    Adicionando mais modelos

--- WebApplication1/Models/Alunos.cs
+++ WebApplication1/Models/Alunos.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Sobrenome { get; set; }
-        public DateTime Matricula { get; set; }
-        public int Idade { get; set; }
+        public DateTime InscricaoData { get; set; }
+        public ICollection<Inscricao> Inscricao {get; set;}
 
     }
 }
---
+++ WebApplication1/Models/Inscricao.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class Inscricao
+    {
+        public enum Grade
+        {
+            A, B, C, D, E
+        }
+        public int InscricaoId { get; set; }
+        public int CursosId { get; set; }
+        public int AlunosId { get; set;}
+        public Grade? GradeCurricular { get; set; }
+        public Cursos Cursos { get; set; }
+        public Alunos Alunos { get; set; }
+    }
+}
