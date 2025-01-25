d04308062360c0b2be04aba319fdf5098bca4ced
CezarEngenheiroDeSoftware <cezar.audelania@gmail.com>
2025-01-25 09:50:44 -0300

    No modelo de Alunos incrementou mais um dado

--- WebApplication1/Models/Alunos.cs
+++ WebApplication1/Models/Alunos.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string Sobrenome { get; set; }
         public DateTime Matricula { get; set; }
+        public int Idade { get; set; }
 
     }
 }
