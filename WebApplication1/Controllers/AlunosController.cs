d7d1a72da8326005fa7d45058b28c5212a4c42e8
CezarEngenheiroDeSoftware <cezar.audelania@gmail.com>
2025-02-03 19:44:31 -0300

    Adição de endpoints controller Alunos

---
+++ WebApplication1/Controllers/AlunosController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AlunosController : Controller
+    {
+        public readonly SchoolContext _dbSchoolContext;
+        public AlunosController(SchoolContext schoolContext)
+        {
+            _dbSchoolContext = schoolContext;
+        }
+        // GET: AlunosController1
+        public async Task<IActionResult> Index()
+        {
+            var listar = await _dbSchoolContext.Alunos.ToListAsync();
+            return View(listar);
+        }
+
+        // GET: AlunosController1/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var detalhes = await _dbSchoolContext.Alunos.Include(x => x.Inscricao).FirstOrDefaultAsync(x => x.Id == id);
+            if(detalhes == null)return NotFound();
+            return View(detalhes);
+        }
+
+        // GET: AlunosController1/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: AlunosController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Alunos aluno)
+        {
+            _dbSchoolContext.Alunos.Add(aluno);
+            await _dbSchoolContext.SaveChangesAsync();
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(aluno);
+            }
+        }
+
+        // GET: AlunosController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var editar = Details(id);
+            if(editar  == null)
+            {
+                return BadRequest();
+            }
+            id = editar.Id;
+            return View(editar);
+        }
+
+        // POST: AlunosController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AlunosController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AlunosController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Migrations/20250203193438_Initial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InscricaoData = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    CursosId = table.Column<int>(type: "int", nullable: false),
+                    titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Creditos = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.CursosId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inscricao",
+                columns: table => new
+                {
+                    InscricaoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CursosId = table.Column<int>(type: "int", nullable: false),
+                    AlunosId = table.Column<int>(type: "int", nullable: false),
+                    GradeCurricular = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inscricao", x => x.InscricaoId);
+                    table.ForeignKey(
+                        name: "FK_Inscricao_Alunos_AlunosId",
+                        column: x => x.AlunosId,
+                        principalTable: "Alunos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Inscricao_Cursos_CursosId",
+                        column: x => x.CursosId,
+                        principalTable: "Cursos",
+                        principalColumn: "CursosId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricao_AlunosId",
+                table: "Inscricao",
+                column: "AlunosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricao_CursosId",
+                table: "Inscricao",
+                column: "CursosId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inscricao");
+
+            migrationBuilder.DropTable(
+                name: "Alunos");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+        }
+    }
+}
